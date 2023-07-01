304f575e52e4e5dbba5cec73ccd11af01bc60894
Prince Kumar <108582161+Princek25@users.noreply.github.com>
2023-07-01 14:06:05 +0530

    Added: ATM Machine Project

--- src/com/company/ATMMachine.java
+++ src/com/company/ATMMachine.java
@@ -1,2 +1,71 @@
-package com.company;public class ATMMachine {
+package com.company;
+
+import java.util.Scanner;
+
+class ATM {
+    float balance;
+    int pin = 1234;
+
+    public void pin() {
+        System.out.print("Welcome to our ATM, Please Enter your valid Pin - ");
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        if (input == pin) {
+            menu();
+        } else {
+            System.out.println("Invalid Pin!");
+            pin();
+        }
+    }
+
+    public void menu() {
+        System.out.println("Press 1 to view balance");
+        System.out.println("Press 2 for deposit money");
+        System.out.println("Press 3 for withdraw money");
+        System.out.println("Press 4 to exit");
+
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        if (input == 1) {
+            System.out.println("The balance : " + balance);
+            menu();
+        } else if (input == 2) {
+            depositMoney();
+        } else if (input == 3) {
+            withdrawMoney();
+        } else if (input == 4) {
+            System.out.println("Thanks for using our ATM Services");
+        } else {
+            System.out.println("Enter valid Input");
+        }
+    }
+
+    public void depositMoney() {
+        System.out.print("Enter money to deposit : ");
+        Scanner sc = new Scanner(System.in);
+        float input = sc.nextInt();
+        balance = balance + input;
+        System.out.println("Your deposited money : " + balance);
+        menu();
+    }
+
+    public void withdrawMoney() {
+        System.out.print("Enter money for withdraw : ");
+        Scanner sc = new Scanner(System.in);
+        float input = sc.nextInt();
+        if (input > balance) {
+            System.out.println("insufficient balance");
+        } else {
+            balance = balance - input;
+            System.out.println("Remaining amount : " + balance);
+        }
+        menu();
+    }
+}
+
+public class ATMMachine {
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        atm.pin();
+    }
 }
